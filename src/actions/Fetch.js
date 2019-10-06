9d68a3e053e390ffa240f3c2eb2d514642165f8c
PavoServices <dg2358@my.open.ac.uk>
2019-10-06 13:29:41 +0100

    Updated to include support for alternative data structures.

--- src/actions/Fetch.js
+++ src/actions/Fetch.js
@@ -1,70 +1,73 @@
-import Axios from '../orm/axios';
-import Action from './Action'
-import Context from '../common/context'
 import isPlainObject from 'lodash/isPlainObject';
+import Axios from '../orm/axios';
+import Action from './Action';
+import Context from '../common/context';
 
 export default class Fetch extends Action {
-    /**
+  /**
      * Call $fetch method
      * @param {object} store
      * @param {object} params
      */
-    static async call({state, commit}, params = {}) {
-        const context = Context.getInstance();
-        const model = context.getModelFromState(state);
-        const endpoint = Action.transformParams('$fetch', model, params);
-        const axios = new Axios(model.methodConf.http);
-        const method = Action.getMethod('$fetch', model, 'get');
-        const request = axios[method](endpoint);
+  static async call({ state, commit }, params = {}) {
+    const context = Context.getInstance();
+    const model = context.getModelFromState(state);
+    const endpoint = Action.transformParams('$fetch', model, params);
+    const axios = new Axios(model.methodConf.http);
+    const method = Action.getMethod('$fetch', model, 'get');
+    const request = axios[method](endpoint);
 
-        this.onRequest(commit);
-        try {
-            await this.onSuccess(commit, model, await request);
-        } catch (error) {
-            this.onError(commit, error);
-        }
-
-        return request;
+    this.onRequest(commit);
+    try {
+      await this.onSuccess(commit, model, await request);
+    } catch (error) {
+      this.onError(commit, error);
     }
 
-    /**
+    return request;
+  }
+
+  /**
      * On Request Method
      * @param {object} commit
      */
-    static onRequest(commit) {
-        commit('onRequest');
-    }
+  static onRequest(commit) {
+    commit('onRequest');
+  }
 
-    /**
+  /**
      * On Successful Request Method
      * @param {object} commit
      * @param {object} model
      * @param {object} data
      */
-    static onSuccess(commit, model, data) {
-        commit('onSuccess')
-        let insertData
-        if (isPlainObject(data)) {
-            const rootKey = model.rootKey || 'data'
-            insertData = data[rootKey]
-        } else {
-            insertData = data
-        }
-
-        return model.insertOrUpdate({
-            data: insertData,
-            meta: {
-                pages: 1
-            }
-        });
+  static onSuccess(commit, model, data) {
+    commit('onSuccess');
+    let insertData;
+    if (isPlainObject(data)) {
+      const rootKey = model.rootKey || 'data';
+      insertData = data[rootKey];
+    } else {
+      insertData = data;
     }
 
-    /**
+    model.commit(state => {
+        state.meta = {
+            pages: 1
+        }
+    });
+
+    return model.insertOrUpdate({
+      data: insertData,
+    });
+  }
+
+  /**
      * On Failed Request Method
      * @param {object} commit
      * @param {object} error
      */
-    static onError(commit, error) {
-        commit('onError', error)
-    }
+  static onError(commit, error) {
+    commit('onError', error);
+  }
 }
